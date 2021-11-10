26a4f68ce54675a5f9b4862860f7d79a2bec0945
samiklaz <samuelasuquo80@gmail.com>
2021-11-10 09:23:35 -0800

    make it better

--- App.js
+++ App.js
@@ -1,21 +1,110 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {Image, TouchableOpacity} from 'react-native'
+import {NavigationContainer, DefaultTheme} from "@react-navigation/native"
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// screens
+import {Onboarding, DestinationDetail} from './screens/Index'
+import {COLORS, SIZES, icons, images, FONTS } from './constants/index'
+
+
+import Tabs from './navigation/tabs'
+
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    border: "transparent",
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator()
+
+const App  = () => {
+  return (
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator initialRouteName={'Onboarding'}>
+        <Stack.Screen 
+          name="Onboarding" 
+          component={Onboarding} 
+          options={{
+            title: null, 
+            headerStyle: {
+            backgroundColor: COLORS.white
+            },
+            headerLeft: null,
+            headerRight: () => (
+              <TouchableOpacity 
+                style={{marginRight: SIZES.padding}}
+                onPress={() => console.log("Pressed")}
+              >
+                <Image
+                  source={icons.barMenu}
+                  resizeMode="contain"
+                  style={{
+                    width: 25,
+                    height: 25,
+                  }}
+                />
+
+            </TouchableOpacity>
+            )
+        }} 
+        />
+
+        <Stack.Screen
+            name="Home"
+            component={Tabs}
+            options={{
+                title: null,
+                headerStyle: {
+                    backgroundColor: COLORS.white
+                },
+                headerLeft: ({ onPress }) => (
+                    <TouchableOpacity
+                        style={{ marginLeft: SIZES.padding }}
+                        onPress={onPress}
+                    >
+                        <Image
+                            source={icons.back}
+                            resizeMode="contain"
+                            style={{
+                                width: 25,
+                                height: 25,
+                            }}
+                        />
+                    </TouchableOpacity>
+                ),
+                headerRight: () => (
+                    <TouchableOpacity
+                        style={{ marginRight: SIZES.padding }}
+                        onPress={() => console.log("Menu")}
+                    >
+                        <Image
+                            source={icons.menu}
+                            resizeMode="contain"
+                            style={{
+                                width: 25,
+                                height: 25,
+                            }}
+                        />
+                    </TouchableOpacity>
+                ),
+            }}
+        />
+
+        <Stack.Screen 
+          name="DestinationDetail"
+          component={DestinationDetail}
+          options = {{
+            headerShown: false
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+
+export default App
---
+++ screens/Index.jsx
@@ -0,0 +1,6 @@
+import Onboarding from './Onboarding'
+import Home from './Home'
+import DestinationDetail from './DestinationDetail'
+
+
+export {Home, Onboarding, DestinationDetail}
